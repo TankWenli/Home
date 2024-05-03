354bf615e5a8fdd1f2a4f08265feeaec43a6af08
TankWenli <141542857+TankWenli@users.noreply.github.com>
2024-05-04 00:38:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -37,6 +37,30 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+$(document).ready(function () {
+    // 捲軸偵測距離頂部超過 50 才顯示按鈕
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop() > 50) {
+        if ($(".back-top").hasClass("hide")) {
+          $(".back-top").toggleClass("hide");
+        }
+      } else {
+        $(".back-top").addClass("hide");
+      }
+    });
+  
+    // 點擊按鈕回頂部
+    $(".back-top").on("click", function (event) {
+      $("html, body").animate(
+        {
+          scrollTop: 0
+        },
+        500 // 回頂部時間為 500 毫秒
+      );
+    });
+  });
+  
+
 // 產品JS
 function changeContent(flowerType) {
     const freshFlower = document.getElementById('freshFlower');
